921f7b4fc0c65531ee84e0d4cdf2279c591de91b
BURAKES26 <70806639+BURAKES26@users.noreply.github.com>
2021-06-10 13:18:27 +0200

    Dl 58 (#4)
    * Testing menu topbar
    
    * Rewrote javascript and changed colors
    
    * Test this ...
    
    * changes
    
    * Removed image and made smaller
    
    * Renamed files to header
    
    * Updated css and html to better standard
    
    * Moved files around
    
    * Removed header css
    
    Co-authored-by: ExploHash <jbtwaalf@gmail.com>

--- header.php
+++
@@ -1,1 +0,0 @@
-<h1>Een mooie header </h1>
---
+++ include/header.php
@@ -0,0 +1,18 @@
+<header>
+  <nav>
+    <ul class="list">
+      <li>Menu Item 1</li>
+      <li>Menu Item 2</li>
+      <li>Menu Item 3</li>
+      <li>Menu Item 4</li>
+      <li>Menu Item 5</li>
+    </ul>
+  </nav>
+  <div id="hamburger">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  <h1>GOALR.</h1>
+  <a href="/auth.php"><img src="images/login.png" size=200%></a>
+</header>
