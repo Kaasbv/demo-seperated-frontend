df10a2a2b5d8eeb6bfc5e8ac691139359c024375
ExploHash <jbtwaalf@gmail.com>
2021-06-12 12:00:57 +0200

    Fixed redirect

--- js/Authlogin.js
+++ js/Authlogin.js
@@ -1,7 +1,7 @@
 function postLogin(event) {
 	event.preventDefault();
 	$.post("/api/user/login.php", $("#login").serialize(), function (data) {
-		window.location.replace("main.php");
+		window.location.replace("/");
 	})
 	.fail(function() { 
 		document.getElementById("error").innerHTML = "Verkeerd gebruikersnaam of wachtwoord";
