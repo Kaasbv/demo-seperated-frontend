4472c69a2ff52a31b419234ab2cbb9f55de37614
steef96 <steef96.sb@gmail.com>
2021-06-12 11:46:13 +0200

    main page functionality (#5)
    * main page functionality
    
    * u happy now jimmy
    
    * fixed some css
    
    * Fixed few things and formatted, and moved css
    
    Co-authored-by: ExploHash <jbtwaalf@gmail.com>

--- include/globalhead.php
+++ include/globalhead.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/global.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
 <script defer src="js/header.js"></script>
