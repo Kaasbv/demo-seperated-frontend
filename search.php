698aa5d1941a8c89814b6d74c39b278c102ada8f
JBeaart <jimmy@levarne.nl>
2021-06-16 14:59:42 +0200

    Added search page (#13)

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php include_once(__DIR__ . "/_modules/TranslationHelper.php") ?>
+<!doctype html>
+<html>
+
+<head>
+  <?php include_once(__DIR__ . "/include/globalhead.php") ?>
+  <?php $ts->loadJSData() ?>
+  <title>Profielpagina</title>
+  <link rel="stylesheet" href="css/search.css">
+  <script src="js/search.js"></script>
+</head>
+
+<body>
+  <?php include_once(__DIR__ . "/include/header.php") ?>
+  <main>
+    <input id="term" type="text" placeholder="<?= $ts->searchPlaceholder ?>">
+    <div id="goals">
+      <div class="center"><?= $ts->goalsNotFound ?></div>
+    </div>
+  </main>
+</body>
+
+</html>
